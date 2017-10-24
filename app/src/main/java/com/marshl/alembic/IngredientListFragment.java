a0995f6f68d90d8f5bf615b6d1f200f8414f2831
marshl <marshl@live.com.au>
2017-10-24 18:31:13 +1100

    Added the ability to select all or select no ingredients by long clicking an ingredient

--- app/src/main/java/com/marshl/alembic/IngredientListFragment.java
+++ app/src/main/java/com/marshl/alembic/IngredientListFragment.java
@@ -1,13 +1,18 @@
 package com.marshl.alembic;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ExpandableListView;
 
-public class IngredientListFragment extends Fragment {
+public class IngredientListFragment extends Fragment implements DialogInterface.OnClickListener {
 
     private AlchemyGame game;
     private IngredientListAdapter ingredientListAdapter;
@@ -36,6 +41,31 @@ public class IngredientListFragment extends Fragment {
         this.ingredientListAdapter = new IngredientListAdapter(this.getActivity(), this.game);
         this.ingredientListView.setAdapter(this.ingredientListAdapter);
 
+        this.ingredientListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                if (ExpandableListView.getPackedPositionType(id) == ExpandableListView.PACKED_POSITION_TYPE_CHILD) {
+                    int groupPosition = ExpandableListView.getPackedPositionGroup(id);
+                    int childPosition = ExpandableListView.getPackedPositionChild(id);
+
+                    final Resources res = getResources();
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                    builder.setMessage("Select an Option");
+                    builder.setPositiveButton("Select all", IngredientListFragment.this)
+                            .setNeutralButton("Select None", IngredientListFragment.this)
+                            .setNegativeButton("Cancel", IngredientListFragment.this);
+                    builder.show();
+
+
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
         this.ingredientListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
             @Override
             public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
@@ -60,4 +90,28 @@ public class IngredientListFragment extends Fragment {
         this.ingredientListAdapter.notifyDataSetChanged();
     }
 
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        Log.d("Alembic", "Which: " + which);
+        switch (which) {
+            case DialogInterface.BUTTON_POSITIVE:
+                this.setAllIngredientSelection(true);
+                break;
+            case DialogInterface.BUTTON_NEUTRAL:
+                this.setAllIngredientSelection(false);
+                break;
+            case DialogInterface.BUTTON_NEGATIVE:
+                break;
+        }
+    }
+
+    private void setAllIngredientSelection(boolean selected) {
+        for (AlchemyPackage pack : this.game.packages) {
+            for (Ingredient ingred : pack.ingredients) {
+                ingred.selected = selected;
+            }
+        }
+
+        this.ingredientListAdapter.notifyDataSetChanged();
+    }
 }
